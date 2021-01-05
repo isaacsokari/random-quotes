2adb4ac1662471d7eb4fc5bf1cddf5facabacda2
Isaac Sokari <isaacsokari@gmail.com>
2021-01-05 12:33:44 +0100

    mode code from codepen to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import quotes from './data/quotes'
 import './App.css';
 
-function App() {
+const getRandomQuote = () => {
+  const quoteIndex = Math.floor(Math.random() * quotes.length);
+
+  return quotes[quoteIndex];
+};
+
+const App = () => {
+  const [currentQuote, setCurrentQuote] = React.useState(getRandomQuote());
+
+  const {quote, author} = currentQuote;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div id="quote-box">
+        <i className="fa fa-quote-left fa-2x"></i>
+        <p id="text">{quote}</p>
+        <i className="fa fa-quote-right fa-2x"></i>
+        
+        <span id="author">- {author}</span>
+
+        <div className='bottom'>
+        <div className="share-links">
+          <a href={`https://www.twitter.com/intent/tweet?text=${encodeURIComponent('"' + quote + '" - ' + author)}`} target="_blank" rel='noreferrer' id="tweet-quote">
+            <i className="fab fa-twitter"></i> 
+          </a>
+        </div>
+        <button
+          id="new-quote"
+          onClick={() => setCurrentQuote(getRandomQuote())}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          New Quote
+        </button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
